a6719c8a688203f9c2c5048c52aa0f2d37459dda
Peti <49650243+Petiazediter@users.noreply.github.com>
2021-03-03 18:53:44 +0100

    Initial commit

---
+++ src/main/java/com/codecool/framybackend/FramybackendApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.framybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FramybackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FramybackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codecool/framybackend/FramybackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.framybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FramybackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
